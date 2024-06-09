dd8a44bc9f9822e10096ed365d54c92823321406
atippaz <nasakun13201@gmail.com>
2024-06-10 01:35:07 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,8 @@ class AppModule {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: '*' });
+  await app.listen(5000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
