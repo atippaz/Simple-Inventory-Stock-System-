19f64e2e6ffbeb62846ba478d8d40f9d5172122c
atippaz <nasakun13201@gmail.com>
2024-06-12 21:53:53 +0700

    update index lading page

--- nuxt3/pages/index.vue
+++ nuxt3/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <div>welcome to Inventory System</div>
-    <div>this program have function</div>
-    <div>1: <a href="/inventory">Inventory</a></div>
-    <div>2: <a href="/requisition">Requisition</a></div>
-    <div>3: <a href="/receiption">Receipt</a></div>
+    <div class="text-8xl font-bold text-center">
+      welcome to Inventory System
+    </div>
+    <div class="text-xl font-bold text-center">this program have function</div>
+    <div class="text-6xl">1: <a href="/inventory">Inventory</a></div>
+    <div class="text-6xl">2: <a href="/requisition">Requisition</a></div>
+    <div class="text-6xl">3: <a href="/receiption">Receipt</a></div>
   </div>
 </template>
